deeda36dc7bad93f782b2d8f59a2d52925ace273
jinfeng <jinfeng@qima-inc.com>
2015-08-31 16:20:23 +0800

    add file

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function (source) {
+  this.cacheable && this.cacheable();
+
+  var content = source.replace(/^\s*module.exports\s*=\s*/, '');
+  var content = content.replace(/;\s*$/, '');
+
+  console.log(content);
+
+  return "var _ = require('underscore'); \n module.exports = _.template(" + content + ")";
+}
